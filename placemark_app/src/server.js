1d0881b37fa0c319a9edb81997b9720f75537a1c
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-21 08:06:15 +0000

    feat: added session-based authentication with @hapi/cookie

--- placemark_app/src/server.js
+++ placemark_app/src/server.js
@@ -1,10 +1,12 @@
 import Hapi from "@hapi/hapi";
 import Vision from "@hapi/vision";
+import Cookie from "@hapi/cookie";
 import Handlebars from "handlebars";
 import path from "path";
 import { fileURLToPath } from "url";
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
+import { accountsController} from "./controllers/accounts-controller.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,6 +17,7 @@ async function init() {
     host: "localhost",
   });
   await server.register(Vision);
+  await server.register(Cookie);
   server.views({
     engines: {
       hbs: Handlebars,
@@ -26,6 +29,18 @@ async function init() {
     layout: true,
     isCached: false,
   });
+
+   server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: COOKIE_NAME_HERE,
+      password: COOKIE_EMCRYPTION_KEY_HERE_MUST_BE_32_CHARS,
+      isSecure: false,
+    },
+    redirectTo: "/",
+    validate: accountsController.validate,
+  });
+  server.auth.default("session");
+
   db.init();
   server.route(webRoutes);
   await server.start();
