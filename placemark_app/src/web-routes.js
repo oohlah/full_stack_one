bc2038141e91259fe4449fa6a74f75d086b2b2c5
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-21 14:15:19 +0000

    fix: fixed typo in addPlacemark route

--- placemark_app/src/web-routes.js
+++ placemark_app/src/web-routes.js
@@ -16,6 +16,6 @@ export const webRoutes = [
   { method: "GET", path: "/dashboard/deletecategory/{id}", config: dashboardController.deleteCategory },
 
    { method: "GET", path: "/category/{id}", config: categoryController.index},
-   { method: "POST", path: "/category/{{id}}/addplacemark", config: categoryController.addPlacemark },
+   { method: "POST", path: "/category/{id}/addplacemark", config: categoryController.addPlacemark },
 
 ];
