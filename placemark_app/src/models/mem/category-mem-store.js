03873472db53c22991eef1b8bfc78bb0ca379aac
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-21 08:49:27 +0000

    feat: mem-store added for categories

---
+++ placemark_app/src/models/mem/category-mem-store.js
@@ -0,0 +1,28 @@
+import { v4 } from "uuid";
+
+let categories = [];
+
+export const categoryMemStore = {
+  async getAllCategories() {
+    return categories;
+  },
+
+  async addCategory(category) {
+    category._id = v4();
+    categories.push(category);
+    return category;
+  },
+
+  async getCategoryById(id) {
+    return categories.find((category) => category._id === id);
+  },
+
+  async deleteCategoriesById(id) {
+    const index = categories.findIndex((category) => category._id === id);
+    categories.splice(index, 1);
+  },
+
+  async deleteAllCategories() {
+    categories = [];
+  },
+};
