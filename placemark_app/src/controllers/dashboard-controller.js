4e02d2f4c9a8abc1642999498ba4734a5165e539
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-21 07:10:35 +0000

    feat: basic template for dashbaordController

--- placemark_app/src/controllers/dashboard-controller.js
+++ placemark_app/src/controllers/dashboard-controller.js
@@ -4,10 +4,29 @@ export const dashboardController = {
       // get pois
       const viewData = {
         title: "POI Dashboard",
-        poi: pois,
+        // categore: category,
       };
       return h.view("dashboard-view", viewData);
     },
 
+  },
+  addCategory: {
+    handler: async function (request, h) {
+      const loggedInUser = request.auth.credentials;
+      const newCategory = {
+        userid: loggedInUser._id,
+        title: request.payload.title,
+      };
+      await db.categoryStore.addCategory(newCategory);
+      return h.redirect("/dashboard");
+    
+  },
+
+  deleteCategory: {
+    handler: async function (request, h) {
+      const category = await db.categoryStore.getCategoryById(request.params.id);
+      await db.categoryStore.deleteCategoryById(category._id);
+      return h.redirect("/dashboard");
+    },
   },
 };
