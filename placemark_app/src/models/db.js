e0eb95c74e491375bcc2e8ba30406e637924dc7a
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-21 13:52:54 +0000

    feat: added placemarkStore to db object

--- placemark_app/src/models/db.js
+++ placemark_app/src/models/db.js
@@ -1,13 +1,15 @@
 import { userMemStore } from "./mem/user-mem-store.js";
 import { categoryMemStore } from "./mem/category-mem-store.js"
+import { placemarkMemStore } from "./mem/placemark-mem-store.js";
 
 export const db = {
   userStore: null,
   categoryStore: null,
-  
+  placemarkStore: null,
 
   init() {
     this.userStore = userMemStore;
     this.categoryStore = categoryMemStore;
+    this.placemarkStore = placemarkMemStore;
   },
 };
