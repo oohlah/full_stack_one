c11e12b914bac308c01eb35795fbeb2a8b1f58af
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-21 08:14:47 +0000

    fix: typo fixed

--- placemark_app/src/controllers/dashboard-controller.js
+++ placemark_app/src/controllers/dashboard-controller.js
@@ -21,7 +21,7 @@ export const dashboardController = {
       return h.redirect("/dashboard");
     
   },
-
+  },
   deleteCategory: {
     handler: async function (request, h) {
       const category = await db.categoryStore.getCategoryById(request.params.id);
