c12f84ae7375e16bea8eab8c1a2913615aa63a3b
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-26 12:14:17 +0000

    fix: corrected typo in function name - getPlacemarkByCategoryId

--- placemark_app/src/models/mongo/placemark-mongo-store.js
+++ placemark_app/src/models/mongo/placemark-mongo-store.js
@@ -1,7 +1,7 @@
 import { Placemark } from "./placemark.js";
 
 export const placemarkMongoStore = {
-  async getPlacemarksByCateogryId(id) {
+  async getPlacemarksByCategoryId(id) {
     const placemarks = await Placemark.find({ categoryid: id }).lean();
     return placemarks;
   },
