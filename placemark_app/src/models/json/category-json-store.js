405c2ff8102902621eac6b454de677a7d3ecf7a3
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-26 09:22:33 +0000

    feat: crud functions added to categoryJsonStore

---
+++ placemark_app/src/models/json/category-json-store.js
@@ -0,0 +1,42 @@
+import { v4 } from "uuid";
+import { db } from "./store-utils.js";
+import { placemarkJsonStore } from "./placemark-json-store.js";
+
+export const categoryJsonStore = {
+  async getAllCategories() {
+    await db.read();
+    return db.data.categories;
+  },
+
+  async addCategory(category) {
+    await db.read();
+    category._id = v4();
+    db.data.categories.push(category);
+    await db.write();
+    return category;
+  },
+
+  async getCategoryById(id) {
+    await db.read();
+    const list = db.data.categories.find((category) => category._id === id);
+    list.placemarks = await placemarkJsonStore.getPlacemarksByCategoryId(list._id);
+    return list;
+  },
+
+  async getUserCategories(userid) {
+    await db.read();
+    return db.data.categories.filter((category) => category.userid === userid);
+  },
+
+  async deleteCategoryById(id) {
+    await db.read();
+    const index = db.data.categories.findIndex((category) => category._id === id);
+    db.data.categories.splice(index, 1);
+    await db.write();
+  },
+
+  async deleteAllCategories() {
+    db.data.categories = [];
+    await db.write();
+  },
+};
