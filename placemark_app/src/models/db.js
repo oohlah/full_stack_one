78cfe8e075368e1a627d31bd1cf332bda8c0318f
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-26 09:14:50 +0000

    feat: placemarkJsonStore added to db object

--- placemark_app/src/models/db.js
+++ placemark_app/src/models/db.js
@@ -2,6 +2,7 @@ import { userMemStore } from "./mem/user-mem-store.js";
 import { categoryMemStore } from "./mem/category-mem-store.js"
 import { placemarkMemStore } from "./mem/placemark-mem-store.js";
 import { userJsonStore } from "./json/user-json-store.js";
+import { placemarkJsonStore} from "./json/placemark-json-store.js";
 
 export const db = {
   userStore: null,
@@ -12,6 +13,7 @@ export const db = {
     switch (storeType) {
       case "json" :
         this.userStore = userJsonStore;
+        this.placemarkStore = placemarkJsonStore;
         break;
       default :
        this.userStore = userMemStore;
