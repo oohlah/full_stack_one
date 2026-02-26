cb976d3e19861ab1935f64ed82f422c58bf20687
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-26 11:58:13 +0000

    feat: added mongo crud functions

---
+++ placemark_app/src/models/mongo/category-mongo-store.js
@@ -0,0 +1,44 @@
+import Mongoose from "mongoose";
+import { Category } from "./category.js";
+// import { trackMongoStore } from "./track-mongo-store.js";
+
+export const categoryMongoStore = {
+  async getAllCategories() {
+    const categories = await Category.find().lean();
+    return categories;
+  },
+
+  async getCategoryById(id) {
+    if (Mongoose.isValidObjectId(id)) {
+      const category = await Category.findOne({ _id: id }).lean();
+      if (category) {
+        // category.placemarks = await placemarkMongoStore.getPlacemarksByCategoryId(category._id);
+      }
+      return category;
+    }
+    return null;
+  },
+
+  async addCategory(category) {
+    const newCategory = new Category(category);
+    const categoryObj = await newCategory.save();
+    return this.getCategoryById(categoryObj._id);
+  },
+
+  async getUserCategories(id) {
+    const category = await Category.find({ userid: id }).lean();
+    return category;
+  },
+
+  async deleteCategoryById(id) {
+    try {
+      await Category.deleteOne({ _id: id });
+    } catch (error) {
+      console.log("bad id");
+    }
+  },
+
+  async deleteAllCategories() {
+    await Category.deleteMany({});
+  },
+};
