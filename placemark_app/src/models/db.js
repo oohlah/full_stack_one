5afb3b471f627634bac8f3c54710e145a72f10fb
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-26 08:59:55 +0000

    feat: db store object modified to accept json & userJsonStore imported

--- placemark_app/src/models/db.js
+++ placemark_app/src/models/db.js
@@ -1,15 +1,22 @@
 import { userMemStore } from "./mem/user-mem-store.js";
 import { categoryMemStore } from "./mem/category-mem-store.js"
 import { placemarkMemStore } from "./mem/placemark-mem-store.js";
+import { userJsonStore } from "./json/user-json-store.js";
 
 export const db = {
   userStore: null,
   categoryStore: null,
   placemarkStore: null,
 
-  init() {
-    this.userStore = userMemStore;
-    this.categoryStore = categoryMemStore;
-    this.placemarkStore = placemarkMemStore;
-  },
+    init(storeType) {
+    switch (storeType) {
+      case "json" :
+        this.userStore = userJsonStore;
+        break;
+      default :
+       this.userStore = userMemStore;
+       this.categoryStore = categoryMemStore;
+       this.placemarkStore = placemarkMemStore;
+  }
+}
 };
