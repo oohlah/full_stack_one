0e1e7578258f58b6c3b29b39abc8e759a56a0434
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-27 06:29:03 +0000

    feat: add createUser helper using axios for API tests

--- placemark_app/test/api/placemark-service.js
+++ placemark_app/test/api/placemark-service.js
@@ -1,3 +1,12 @@
-export const placemarkService = {
+import axios from "axios";
 
+import { serviceUrl } from "../fixtures.js";
+
+export const placemarkService = {
+placemarkUrl: serviceUrl,
+
+ async createUser(user) {
+    const res = await axios.post(`${this.placemarkUrl}/api/users`, user);
+    return res.data;
+  }
 };
