74e58ab5925a27a562f083bea1d83d6cb9c2ee79
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-27 07:20:37 +0000

    feat: added structure for category api tests

---
+++ placemark_app/test/api/category-api-tests.js
@@ -0,0 +1,13 @@
+import { assert } from "chai";
+import { placemarkService } from "./placemark-service.js";
+import { maggie, river,testCategories } from "../fixtures.js";
+import { assertSubset } from "../test-utils.js";
+
+suite("User API tests", () => {
+  setup(async () => {
+  });
+  teardown(async () => {
+  });
+
+
+  });
