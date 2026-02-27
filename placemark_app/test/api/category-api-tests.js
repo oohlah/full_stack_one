59270206afb44ad586bcd3bf38c537ef7b9c6405
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-27 08:06:49 +0000

    test: modified api users tests - all passing

--- placemark_app/test/api/category-api-tests.js
+++ placemark_app/test/api/category-api-tests.js
@@ -3,7 +3,7 @@ import { placemarkService } from "./placemark-service.js";
 import { maggie, river,testCategories } from "../fixtures.js";
 import { assertSubset } from "../test-utils.js";
 
-suite("User API tests", () => {
+suite("User Category tests", () => {
   setup(async () => {
   });
   teardown(async () => {
--- placemark_app/test/api/user-api-test.js
+++ placemark_app/test/api/user-api-test.js
@@ -2,13 +2,15 @@ import { assert } from "chai";
 import { placemarkService } from "./placemark-service.js";
 import { maggie, testUsers } from "../fixtures.js";
 import { assertSubset } from "../test-utils.js";
+import { db } from "../../src/models/db.js";
 
 suite("User API tests", () => {
   setup(async () => {
-   await db.userStore.deleteAll();
+     db.init();
+   await placemarkService.deleteAllUsers();
        for (let i = 0; i < testUsers.length; i += 1) {
          // eslint-disable-next-line no-await-in-loop
-         testUsers[i] = await db.userStore.addUser(testUsers[i]);
+         testUsers[i] = await placemarkService.createUser(testUsers[i]);
        }
   });
   teardown(async () => {
@@ -29,6 +31,11 @@ suite("User API tests", () => {
       assert.equal(error.response.data.statusCode, 404);
     }
   });
+      test("get a user", async () => {
+    const returnedUser = await placemarkService.getUser(testUsers[0]._id);
+    assertSubset(testUsers[0], returnedUser);
+    
+  });
 
     test("get a user - deleted user", async () => {
     await placemarkService.deleteAllUsers();
@@ -39,5 +46,13 @@ suite("User API tests", () => {
       assert(error.response.data.message === "No User with this id");
       assert.equal(error.response.data.statusCode, 404);
     }
+  });
+
+   test("delete all userApi", async () => {
+    let returnedUsers = await placemarkService.getAllUsers();
+    assert.equal(returnedUsers.length, 3);
+    await placemarkService.deleteAllUsers();
+    returnedUsers = await placemarkService.getAllUsers();
+    assert.equal(returnedUsers.length, 0);
   });
 });
