503b49d250e074514ae86772e32a6ce68ca27ade
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-02-27 06:15:12 +0000

    feat: set up user-api test template

---
+++ placemark_app/test/api/user-api-test.js
@@ -0,0 +1,11 @@
+import { placemarkService } from "./placemark-service.js";
+
+suite("User API tests", () => {
+  setup(async () => {
+  });
+  teardown(async () => {
+  });
+
+  test("create a user", async () => {
+  });
+});
