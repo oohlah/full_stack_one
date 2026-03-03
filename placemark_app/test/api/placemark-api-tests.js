6eba3c19a94e2ccd5eef8734b6ab714207c4f059
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-03-03 05:59:47 +0000

    test: added skeleton for placemarkApi tests

---
+++ placemark_app/test/api/placemark-api-tests.js
@@ -0,0 +1,26 @@
+import { assert } from "chai";
+import { assertSubset } from "../test-utils.js";
+import { placemarkService } from "./placemark-service.js";
+import { maggie, river, testCategories, testPlacemarks, liffey } from "../fixtures.js";
+
+suite("Placemark API tests", () => {
+  let user = null;
+  let museaum = null;
+
+  setup(async () => {
+  });
+
+  teardown(async () => {});
+
+  test("create placemark", async () => {
+  });
+
+  test("create Multiple placemarks", async () => {
+  });
+
+  test("Delete placemark", async () => {
+  });
+
+  test("test denormalised category", async () => {
+  });
+});
