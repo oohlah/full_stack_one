e4701bc8e1fa5eb2ce6aeab38734343fd641da26
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-03-03 10:52:51 +0000

    feat: extend user Joi schemas to support DB fields for testing

--- placemark_app/src/models/joi-schema.js
+++ placemark_app/src/models/joi-schema.js
@@ -1,25 +1,39 @@
 import Joi from "joi";
 
+export const IdSpec = Joi.alternatives().try(Joi.string(), Joi.object()).description("a valid ID");
+
 export const UserSpec = Joi.object()
   .keys({
     firstName: Joi.string().example("Homer").required(),
     lastName: Joi.string().example("Simpson").required(),
     email: Joi.string().email().example("homer@simpson.com").required(),
     password: Joi.string().example("secret").required(),
+     _id: IdSpec,
+    __v: Joi.number(),
   })
   .label("UserDetails");
 
-export const UserArray = Joi.array().items(UserSpec).label("UserArray");
-};
 
-export const UserCredentials = {
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-};
+export const UserCredentialsSpec = Joi.object()
+ .keys({
+    email: Joi.string().email().example("homer@simpson.com").required(),
+    password: Joi.string().example("secret").required(),
+     _id: IdSpec,
+    __v: Joi.number(),
+})
+.label("UserCredentials");
+
+export const UserSpecPlus = UserSpec.keys({
+  _id: IdSpec,
+  __v: Joi.number(),
+}).label("UserDetailsPlus");
+
+export const UserArray = Joi.array().items(UserSpecPlus).label("UserArray");
 
 export const CategorySpec = {
     title: Joi.string().required(),
 };
+
 
 export const placemarkSpec = {
     name: Joi.string().required(),
