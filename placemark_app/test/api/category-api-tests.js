22704138a8ccffc7e05fd0e8911fc93ae8532e9b
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-03-03 07:47:29 +0000

    test: test passed - remove non-existant category

--- placemark_app/test/api/category-api-tests.js
+++ placemark_app/test/api/category-api-tests.js
@@ -34,9 +34,25 @@ suite(" Category Api tests", () => {
   });
 
   test("create multiple categories", async () => {
+     for (let i = 0; i < testCategories.length; i += 1) {
+      testCategories[i].userid = user._id;
+      // eslint-disable-next-line no-await-in-loop
+      await placemarkService.createCategory(testCategories[i]);
+    }
+    let returnedLists = await placemarkService.getAllCategories();
+    assert.equal(returnedLists.length, testCategories.length);
+    await placemarkService.deleteAllCategories();
+    returnedLists = await placemarkService.getAllCategories();
+    assert.equal(returnedLists.length, 0);
   });
 
   test("remove non-existant category", async () => {
+    try {
+      const response = await placemarkService.deleteCategory("not an id");
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message.startsWith("No Category with this id"), "Incorrect Response Message");
+    }
   });
 
   });
