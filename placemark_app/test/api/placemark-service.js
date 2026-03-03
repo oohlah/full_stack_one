bc85371264022a83ccf9797ef2289d00e55da91e
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-03-03 06:09:40 +0000

    feat: extended placemark-service with placemarks endpoints

--- placemark_app/test/api/placemark-service.js
+++ placemark_app/test/api/placemark-service.js
@@ -49,5 +49,29 @@ placemarkUrl: serviceUrl,
   async deleteAllCategories() {
     const res = await axios.delete(`${this.placemarkUrl}/api/categories`);
     return res.data;
+  },
+  async createPlacemark(placemark) {
+    const res = await axios.post(`${this.placemarkUrl}/api/placemarks`, placemark);
+    return res.data;
+  },
+
+  async getPlacemark(id) {
+    const res = await axios.get(`${this.placemarkUrl}/api/placemarks/${id}`);
+    return res.data;
+  },
+
+   async getAllPlacemarks() {
+    const res = await axios.get(`${this.placemarkUrl}/api/placemarks`);
+    return res.data;
+  },
+
+  async deletePlacemark(id) {
+    const res = await axios.delete(`${this.placemarkUrl}/api/placemarks/${id}`);
+    return res.data;
+  },
+  
+  async deleteAllPlacemarks() {
+    const res = await axios.delete(`${this.placemarkUrl}/api/placemarks`);
+    return res.data;
   }
 };
