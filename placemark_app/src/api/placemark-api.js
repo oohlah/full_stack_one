9d86124d1a5a20d3897c313735a8cb119a8d30bf
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-03-03 06:41:10 +0000

    fix: fixed typo in create function - return placemark

--- placemark_app/src/api/placemark-api.js
+++ placemark_app/src/api/placemark-api.js
@@ -17,12 +17,27 @@ export const placemarkApi = {
   create: {
     auth: false,
     handler: async function (request, h) {
+         try {
+        const placemark = await db.placemarkStore.addPlacemark(request.params.id, request.payload);
+        if (placemark) {
+          return h.response(placemark).code(201);
+        }
+        return Boom.badImplementation("error creating placemark");
+      } catch (err) {
+        return Boom.serverUnavailable("Database Error:", err);
+      }
     },
   },
 
   deleteAll: {
     auth: false,
     handler: async function (request, h) {
+        try {
+        await db.placemarkStore.deleteAllPlacemarks();
+        return h.response().code(204);
+      } catch (err) {
+        return Boom.serverUnavailable("Database Error:", err);
+      }
     },
   },
 
