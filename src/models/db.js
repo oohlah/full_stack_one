e81dde94ed9855bbd9ba1cdf5f05f78e8d757322
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-03-13 05:27:30 +0000

    feat: added firbase as database in db object facade

--- src/models/db.js
+++ src/models/db.js
@@ -8,6 +8,10 @@ import { userMongoStore } from "./mongo/user-mongo-store.js";
 import { categoryMongoStore } from "./mongo/category-mongo-store.js";
 import { placemarkMongoStore } from "./mongo/placemark-mongo-store.js";
 import { connectMongo } from "./mongo/connect.js";
+import { userFirebaseStore } from "./firebase/user-firebase-store.js";
+import { categoryFirebaseStore } from "./firebase/category-firebase-store.js";
+import { placemarkFirebaseStore } from "./firebase/placemark-firebase-store.js";
+
 
 export const db = {
   userStore: null,
@@ -26,6 +30,11 @@ export const db = {
         this.categoryStore = categoryMongoStore;
         this.placemarkStore = placemarkMongoStore;
         connectMongo();
+        break;
+       case "firebase" :
+         this.userStore = userFirebaseStore;
+         this.categoryStore = categoryFirebaseStore;
+         this.placemarkStore = placemarkFirebaseStore;
         break;
       default :
        this.userStore = userMemStore;
