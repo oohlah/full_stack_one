06ac4ddf5a16d8c1712e1482b3b1f118cbc1a10c
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-03-14 06:54:06 +0000

    feat: added function in stationController to call updateUserEmail

--- src/controllers/settings-controller.js
+++ src/controllers/settings-controller.js
@@ -44,30 +44,34 @@ export const settingsController = {
     return h.view("settings-view", viewData);
   },
   },
-//   addCategory: {
-//     validate: {
-//           payload: CategorySpec,
-//           options: {abortEarly: false},
-//           failAction: function (request, h, error) {
-//             return h.view("dashboard-view", { title: "Category title error", errors: error.details }).takeover().code(400);
-//           },
-//         },
-//     handler: async function (request, h) {
-//       const loggedInUser = request.auth.credentials;
-//       const newCategory = {
-//         userid: loggedInUser._id,
-//         title: request.payload.title,
-//       };
-//       await db.categoryStore.addCategory(newCategory);
-//       return h.redirect("/dashboard");
+
+    updateEmail: {
+    handler: async function (request, h){
+
+    console.log("Hits updateEmail Controller");
+
+    const user = request.auth.credentials;
     
-//   },
-//   },
-//   deleteCategory: {
-//     handler: async function (request, h) {
-//       const category = await db.categoryStore.getCategoryById(request.params.id);
-//       await db.categoryStore.deleteCategoryById(category._id);
-//       return h.redirect("/dashboard");
-//     },
-//   },
+    console.log("USER TO CHANGE",user);
+
+  
+    // Returns the whole user object
+    const updatedUser = await db.userStore.updateUserEmail(user._id, {
+  email: request.payload.email
+});
+
+    console.log("UPDATED USER: ", updatedUser);
+
+    const viewData = {
+      title: "User Settings",
+      user: updatedUser,
+    };
+    
+    console.log(`Email Changed to: ${updatedUser.email}`);
+    
+    return h.view("settings-view", viewData);
+  },
+  },
+
+
 };
