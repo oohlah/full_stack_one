14f60463cb29062953e54926a0aa9f0c9464c640
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-03-15 15:35:55 +0000

    feat: added adminIndex and makeAdmin for admin dashboard

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -5,15 +5,65 @@ export const dashboardController = {
   index: {
     handler: async function (request, h) {
       const loggedInUser = request.auth.credentials;
+       console.log("Logged in user credentials:", request.auth.credentials);
       const categories = await db.categoryStore.getUserCategories(loggedInUser._id);
+
+       // if user is admin - redirect admin dashboard
+      if (loggedInUser.scope === "admin") {
+      return h.redirect("/dashboard/admin");
+    }
+
       const viewData = {
         title: "POI Dashboard",
         categories: categories,
       };
       return h.view("dashboard-view", viewData);
     },
+ },
+    adminIndex: {
+    handler: async function (request, h) {
+      const loggedInUser = request.auth.credentials;
 
+      console.log("Logged:", request.auth.credentials);
+
+      console.log("ADMIN DASHBOARD");
+      // restrict acces if not admin
+      if (loggedInUser.scope !== "admin") {
+        return h.response("Forbidden").code(403);
+      }
+
+      const users = await db.userStore.getAllUsers();
+      const categories = await db.categoryStore.getAllCategories();
+      const viewData = {
+        title: "Admin Dashboard",
+        users,
+        categories,
+      };
+      return h.view("admin-dashboard-view", viewData);
+    },
+    
   },
+
+  makeAdmin: {
+  handler: async function (request, h) {
+    const userId = request.params.id;
+
+    await db.userStore.setAdmin(userId);
+    
+
+    return h.redirect("/dashboard/admin");
+  },
+},
+
+ deleteUser: {
+  handler: async function (request, h) {
+    const userId = request.params.id;
+
+    await db.userStore.deleteUserById(userId);
+
+    return h.redirect("/dashboard/admin");
+  },
+},
   addCategory: {
     validate: {
           payload: CategorySpec,
